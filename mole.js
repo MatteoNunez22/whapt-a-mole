179cc345f9831b37c7b6235f90ad7f029d4fdef7
matteonm <matteonm@cim.mcgill.ca>
2018-07-27 14:25:54 -0400

    Basic shoe-less working version

--- mole.js
+++ mole.js
@@ -1,6 +1,22 @@
 var mole = {
+    id: "",
+    score: 0
+};
+
+var settings = {
+    intensity: 255,
+    duration: 1000,
+    paused: true,
+    mode: 0 // Singleplayer :   0
+            // Multiplayer  :   1
+};
+
+var pressed = {
     id: ""
 };
+
+var lastIndex = -1;
+var times = 0;
 
 mole.startContainer = function () {
     $('#title').hide();
@@ -8,23 +24,122 @@ mole.startContainer = function () {
     $('#shoeButtons').hide();
     $('.experimentButton').hide();
     $('#moleView').show();
+    mole.status = "Not started";
+    document.getElementById('status').getElementsByTagName('span')[0].innerText = mole.status;
+    //$("#status > #span").html(mole.status);
+
+    mole.score = 0;
+    document.getElementById('score').getElementsByTagName('span')[0].innerText = mole.score;
+    //$("#score > #span").html(mole.score);
 };
 
-$(document).ready(function() {
+$(document).ready(function() {      // Document ready
 
-    mole.start = function () {
+    mole.play = function () {       // Plays one mole
+        //console.log("lastIndex = " + lastIndex);
+        if (lastIndex == -1) {
+            mole.status = "Started";
+            document.getElementById('status').getElementsByTagName('span')[0].innerText = mole.status;
+            //console.log("STARTED");
+            settings.paused = false;
+        }
+        times = 0;
+        mole.randomize();
 
+        if (settings.paused == false) {
+            setTimeout(mole.play, (Math.random() * 1200 + 500));  // Random mole appearence
+        }
     };
 
     mole.pause = function () {
-
+        if (settings.paused) {  // Unpaused
+            settings.paused = false;
+            mole.status = "Started";
+            document.getElementById('status').getElementsByTagName('span')[0].innerText = mole.status;
+            document.getElementById('moleView').getElementsByTagName('button')[1].innerText = "Pause";
+            //console.log("Unpaused");
+            mole.play();
+        } else {                // Paused
+            settings.paused = true;
+            mole.status = "Paused";
+            document.getElementById('status').getElementsByTagName('span')[0].innerText = mole.status;
+            document.getElementById('moleView').getElementsByTagName('button')[1].innerText = "Unpause";
+            //console.log("PAUSED");
+        }
     };
 
     mole.restart = function () {
+        mole.score = 0;
+        document.getElementById('score').getElementsByTagName('span')[0].innerText = mole.score;
+        console.log("RESTART");
 
+        mole.id = "";
+        lastIndex = -1;
+        settings.paused = true;
+        mole.play();
+    };
+
+    mole.randomize = function () {
+        var index = lastIndex;
+        while (lastIndex == index) {
+            index = (Math.random() * 4) | 0;
+        }
+        if (lastIndex = -1) {  // First time
+            //settings.paused = false;
+        }
+
+        lastIndex = index;
+        mole.animate(index);
     };
 
     mole.pressed = function () {
+        //console.log("PRESSED!!!--------------");
+        if (pressed.id === mole.id && times === 0) {
+            // Whacked!!!
+            times++;
+            mole.score++;
+            console.log("Score: " + mole.score);
+            document.getElementById('score').getElementsByTagName('span')[0].innerText = mole.score;
+            console.log("WHACKED!!!-----------------------------------");
+        }
+    };
+
+    mole.animate = function (index) {
+        if (index === 0) {
+            app.sendMessage(" t " + "t " + settings.intensity + " " + settings.duration + "\r");
+            $("#a").css("border-color", "#1aff00");
+            mole.id = "a";
+            setTimeout(function() {
+                $("#a").css("border-color", "#0b7000");
+                mole.id = "";
+            }, settings.duration + 200);
+        } else if (index === 1) {
+            app.sendMessage(" t " + "r " + settings.intensity + " " + settings.duration + "\r");
+            $("#b").css("border-color", "#ff0b00");
+            mole.id = "b";
+            setTimeout(function() {
+                $("#b").css("border-color", "#c30800");
+                mole.id = "";
+            }, settings.duration + 200);
+        } else if (index === 2) {
+            app.sendMessage(" t " + "l " + settings.intensity + " " + settings.duration + "\r");
+            $("#c").css("border-color", "#ffec00");
+            mole.id = "c";
+            setTimeout(function() {
+                $("#c").css("border-color", "#c3b400");
+                mole.id = "";
+            }, settings.duration + 200);
+        } else if (index === 3) {
+            app.sendMessage(" t " + "h " + settings.intensity + " " + settings.duration + "\r");
+            $("#d").css("border-color", "#29abd0");
+            mole.id = "d";
+            setTimeout(function() {
+                $("#d").css("border-color", "#196d85");
+                mole.id = "";
+            }, settings.duration + 200);
+        }
+        console.log("Mole: " + mole.id);
+        console.log("Pressed: " + pressed.id);
 
     };
 
@@ -72,23 +187,22 @@ $(document).ready(function() {
         app.leftShoe = false;
 
         switch(winner) {
-            case 1: mole.id = "c";
+            case 1: pressed.id = "c";
                 break;
 
-            case 2: mole.id = "b";
+            case 2: pressed.id = "b";
                 break;
 
-            case 3: mole.id = "d";
+            case 3: pressed.id = "d";
                 break;
 
-            case 4: mole.id = "a";
+            case 4: pressed.id = "a";
                 break;
 
             default:
                 break;
         }
         mole.pressed();
-
     };
 
     // Listen to left shoe
@@ -135,23 +249,67 @@ $(document).ready(function() {
         app.leftShoe = true;
 
         switch(winner) {
-            case 1: mole.id = "c";
+            case 1: pressed.id = "c";
                 break;
 
-            case 2: mole.id = "b";
+            case 2: pressed.id = "b";
                 break;
 
-            case 3: mole.id = "d";
+            case 3: pressed.id = "d";
                 break;
 
-            case 4: mole.id = "a";
+            case 4: pressed.id = "a";
                 break;
 
             default:
                 break;
         }
         mole.pressed();
-
     };
 
-}); //document ready
+    // SHOE-LESS MOD
+    if (app.leftShoe) {
+        $("#a").on("click", function () {
+            pressed.id = "a";
+            //console.log("Pressed A");
+            mole.pressed();
+        });
+        $("#b").on("click", function () {
+            pressed.id = "b";
+            //console.log("Pressed B");
+            mole.pressed();
+        });
+        $("#c").on("click", function () {
+            pressed.id = "c";
+            //console.log("Pressed C");
+            mole.pressed();
+        });
+        $("#d").on("click", function () {
+            pressed.id = "d";
+            //console.log("Pressed D");
+            mole.pressed();
+        });
+
+    } else {
+        $("#a").on("click", function () {
+            pressed.id = "a";
+            //console.log("Pressed A");
+            mole.pressed();
+        });
+        $("#b").on("click", function () {
+            pressed.id = "b";
+            //console.log("Pressed B");
+            mole.pressed();
+        });
+        $("#c").on("click", function () {
+            pressed.id = "c";
+            //console.log("Pressed C");
+            mole.pressed();
+        });
+        $("#d").on("click", function () {
+            pressed.id = "d";
+            //console.log("Pressed D");
+            mole.pressed();
+        });
+    }
+});
